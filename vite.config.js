a50d73a8ed5fdfe513be1e8ba49afab2d4b5cad4
Abhishek-Sathala <abhisheksathala296@gmail.com>
2024-12-12 08:06:17 +1300

    changed somtting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ui-ux-F/', // Replace 'ui-ux-F' with your repository name
   plugins: [react()],
-})
+});
